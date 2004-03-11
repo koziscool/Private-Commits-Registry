ee9807387254d479522b5845b9c4c96616870f3c
koziscool <kozjob@yahoo.com>
2004-03-11 00:00:00 -0600

    random message

--- run_commit_message.rb
+++ run_commit_message.rb
@@ -4,8 +4,8 @@ require 'time'
 x = Time.new(2004, 3, 11).xmlschema.to_s
 
 
-puts`git add -A`
-puts`git commit -m "random message" --date #{x}`
-puts`git status`
+`git add -A`
+`git commit -m "random message" --date #{x}`
+`git push origin master`
 # %x( cmd )
 
